c7456f2c1084499f6fd568872587bfd156f40751
abubaker.asuliman@gmail.com <abubakersuliman@outlook.com>
2023-05-11 01:10:14 +0400

    reformat user app

--- lunch_decision/UserAuth/views.py
+++ lunch_decision/UserAuth/views.py
@@ -1,10 +1,1 @@
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import AllowAny
-from .models import User
-from .serializers import UserSerializer
 
-
-# class UserListCreateView(ListCreateAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     permission_classes = [AllowAny]

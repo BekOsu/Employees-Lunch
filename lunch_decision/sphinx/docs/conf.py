55b52f95b469805dbacacf88765ce61e38390cbb
abubaker.asuliman@gmail.com <abubakersuliman@outlook.com>
2023-05-12 15:07:27 +0400

    GitHub Pages

--- lunch_decision/sphinx/docs/conf.py
+++ lunch_decision/sphinx/docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../lunch_decision'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'lunch_decision'
 copyright = '2023, abu'
